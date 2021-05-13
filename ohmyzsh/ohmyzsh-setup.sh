c94e01f736b22dd1acdb706b4540ffd0eac94068
Andrew Briscoe <everynothing@gmail.com>
2021-05-13 02:10:18 -0600

    unless stopped

--- ohmyzsh/ohmyzsh-setup.sh
+++ ohmyzsh/ohmyzsh-setup.sh
@@ -10,10 +10,10 @@ echo "sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "wget -q -O \"$HOME/.zsh_aliasfile:///snap/bitwarden/46/resources/app.asar/index.html#es\" \"$REPO_LOC/.zsh_aliases\" ~/.zsh_aliases"
-wget -q -O "$HOME/.zsh_aliases" "$REPO_LOC/.zsh_aliases" ~/.zsh_aliases
+wget -q -O "$HOME/.zsh_aliases" "$REPO_LOC/.zsh_aliases"
 
 echo "wget -q -O \"\$HOME/.zshrc\" \"$REPO_LOC/.zshrc\" ~/.zshrc"
-wget -q -O "$HOME/.zshrc" "$REPO_LOC/.zshrc" ~/.zshrc
+wget -q -O "$HOME/.zshrc" "$REPO_LOC/.zshrc"
 
 THEME_NAME="lambda-mod.zsh-theme"
 echo "wget -O \"\$HOME/.oh-my-zsh/themes/$THEME_NAME\" \"$REPO_LOC/$THEME_NAME\""
