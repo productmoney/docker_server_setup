e9854472b4681d3ebc3ef6601ce92e5b2584e28d
Andrew Briscoe <everynothing@gmail.com>
2021-05-13 01:31:25 -0600

    quote django pw

--- ohmyzsh/ohmyzsh-setup.sh
+++ ohmyzsh/ohmyzsh-setup.sh
@@ -9,7 +9,7 @@ REPO_LOC="https://raw.githubusercontent.com/productmoney/docker_server_setup/mai
 echo "sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "wget -q -O \"$HOME/.zsh_aliases\" \"$REPO_LOC/.zsh_aliases\" ~/.zsh_aliases"
+echo "wget -q -O \"$HOME/.zsh_aliasfile:///snap/bitwarden/46/resources/app.asar/index.html#es\" \"$REPO_LOC/.zsh_aliases\" ~/.zsh_aliases"
 wget -q -O "$HOME/.zsh_aliases" "$REPO_LOC/.zsh_aliases" ~/.zsh_aliases
 
 echo "wget -q -O \"\$HOME/.zshrc\" \"$REPO_LOC/.zshrc\" ~/.zshrc"
