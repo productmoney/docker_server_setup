9a2cc61d1ad11cf9d9c65477cd5d653b381bb113
Andrew Briscoe <everynothing@gmail.com>
2021-05-12 23:56:42 -0600

    more server setup stuff

--- ohmyzsh/ohmyzsh-setup.sh
+++ ohmyzsh/ohmyzsh-setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "Installing gotop cause why not XD"
-sudo snap install gotop
-
 echo "Changing users shell to /bin/zsh."
 echo "Please enter sudo password to do so."
 chsh -s /bin/zsh
 
+REPO_LOC="https://raw.githubusercontent.com/productmoney/docker_server_setup/main/ohmyzsh"
+
+echo "sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-wget -q "https://raw.githubusercontent.com/productmoney/docker_server_setup/main/ohmyzsh/.zsh_aliases" ~/.zsh_aliases
+echo "wget -q -O \"$HOME/.zsh_aliases\" \"$REPO_LOC/.zsh_aliases\" ~/.zsh_aliases"
+wget -q -O "$HOME/.zsh_aliases" "$REPO_LOC/.zsh_aliases" ~/.zsh_aliases
 
-wget -q "https://raw.githubusercontent.com/productmoney/docker_server_setup/main/ohmyzsh/.zshrc" ~/.zshrc
+echo "wget -q -O \"\$HOME/.zshrc\" \"$REPO_LOC/.zshrc\" ~/.zshrc"
+wget -q -O "$HOME/.zshrc" "$REPO_LOC/.zshrc" ~/.zshrc
 
-wget -q "https://raw.githubusercontent.com/productmoney/docker_server_setup/main/ohmyzsh/lambda-mod.zsh-theme" ~/.ohmyzsh/themes
+THEME_NAME="lambda-mod.zsh-theme"
+echo "wget -O \"\$HOME/.oh-my-zsh/themes/$THEME_NAME\" \"$REPO_LOC/$THEME_NAME\""
+wget -O "$HOME/.oh-my-zsh/themes/$THEME_NAME" "$REPO_LOC/$THEME_NAME"
