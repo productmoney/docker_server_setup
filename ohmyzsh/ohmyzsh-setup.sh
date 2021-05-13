38c72c0adcebb30bb516a60e2fe9cc5c0a5a1042
Andrew Briscoe <everynothing@gmail.com>
2021-05-13 04:08:10 -0600

    next zsh step

--- ohmyzsh/ohmyzsh-setup.sh
+++ ohmyzsh/ohmyzsh-setup.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-#echo "Changing users shell to /bin/zsh."
-#echo "Please enter sudo password to do so."
-#chsh -s /bin/zsh
-
-REPO_LOC="https://raw.githubusercontent.com/productmoney/docker_server_setup/main/ohmyzsh"
-
-echo "wget -q -O \"$HOME/.zsh_aliasfile:///snap/bitwarden/46/resources/app.asar/index.html#es\" \"$REPO_LOC/.zsh_aliases\" ~/.zsh_aliases"
-wget -q -O "$HOME/.zsh_aliases" "$REPO_LOC/.zsh_aliases"
-
-echo "wget -q -O \"\$HOME/.zshrc\" \"$REPO_LOC/.zshrc\" ~/.zshrc"
-wget -q -O "$HOME/.zshrc" "$REPO_LOC/.zshrc"
-
-THEME_NAME="lambda-mod.zsh-theme"
-echo "wget -O \"\$HOME/.oh-my-zsh/themes/$THEME_NAME\" \"$REPO_LOC/$THEME_NAME\""
-wget -O "$HOME/.oh-my-zsh/themes/$THEME_NAME" "$REPO_LOC/$THEME_NAME"
-
 echo "sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
---
+++ ohmyzsh/zsh_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO_LOC="https://raw.githubusercontent.com/productmoney/docker_server_setup/main/ohmyzsh"
+
+echo "wget -q -O \"$HOME/.zsh_aliasfile:///snap/bitwarden/46/resources/app.asar/index.html#es\" \"$REPO_LOC/.zsh_aliases\" ~/.zsh_aliases"
+wget -q -O "$HOME/.zsh_aliases" "$REPO_LOC/.zsh_aliases"
+
+echo "wget -q -O \"\$HOME/.zshrc\" \"$REPO_LOC/.zshrc\" ~/.zshrc"
+wget -q -O "$HOME/.zshrc" "$REPO_LOC/.zshrc"
+
+THEME_NAME="lambda-mod.zsh-theme"
+echo "wget -O \"\$HOME/.oh-my-zsh/themes/$THEME_NAME\" \"$REPO_LOC/$THEME_NAME\""
+wget -O "$HOME/.oh-my-zsh/themes/$THEME_NAME" "$REPO_LOC/$THEME_NAME"
