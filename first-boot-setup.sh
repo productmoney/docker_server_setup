1c61bb38ad9033505b7c8ae29e12f649c5c32f98
Andrew Briscoe <everynothing@gmail.com>
2021-05-17 14:45:24 -0600

    debian fixes

--- first-boot-setup.sh
+++ first-boot-setup.sh
@@ -20,7 +20,7 @@ read -r DESIRED_USERNAME
 echo ""
 adduser --disabled-password --gecos "" "$DESIRED_USERNAME"
 sleep 2
-echo "$DESIRED_USERNAME":"$randompw" | chpasswd
+yes "$randompw" | passwd "$DESIRED_USERNAME"
 echo "Added $DESIRED_USERNAME with the randomized password:"
 echo "$randompw"
 
@@ -34,4 +34,4 @@ sed -i 's/env_reset/env_reset,timestamp_timeout=90/' "/etc/sudoers"
 
 echo ""
 echo "----------"
-echo "Done, please re-login as user account now."
+echo "Done, please re-login as $DESIRED_USERNAME using password $randompw now."
