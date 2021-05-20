df3a0789b48f6b6243a16ab149509eaa9da1c279
Andrew Briscoe <everynothing@gmail.com>
2021-05-20 00:46:12 -0600

    -y

--- install_standard_debian_packages.sh
+++ install_standard_debian_packages.sh
@@ -6,6 +6,6 @@ DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get upgrade -y
 
-apt-get install apt-utils
-apt-get install software-properties-common
+apt-get install -y apt-utils
+apt-get install -y software-properties-common
 apt-get install -y htop wget git ntp zsh apt-transport-https ca-certificates curl rsync
