6539c5303ed8fed3d29b2656aba8500686c67144
Andrew Briscoe <everynothing@gmail.com>
2021-05-20 01:24:37 -0600

    echo in add user

--- add_user.sh
+++ add_user.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "adduser --disabled-password --gecos "" $1"
 adduser --disabled-password --gecos "" "$1"
+
+echo ""
+echo "yes $2 | passwd $1"
 yes "$2" | passwd "$1"
 
-echo "Added $DESIRED_USERNAME with the randomized password: "
-echo "$2"
-
+echo ""
+echo "adduser $1 sudo"
 adduser "$1" sudo
