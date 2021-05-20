9fe4ea23607942f604e508cfac963411b36aecc6
Andrew Briscoe <everynothing@gmail.com>
2021-05-20 03:01:56 -0600

    first boot setup done try

--- first-boot-setup.sh
+++ first-boot-setup.sh
@@ -1,37 +1,67 @@
 #!/bin/bash
 
+USER_PASSWORD_LENGTH=12
+ORGANIZATION="productmoney"
+GH_CONTENT="https://raw.githubusercontent.com/$ORGANIZATION"
+
 export DEBIAN_FRONTEND=noninteractive
+DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get upgrade -y
+randompw=$(date +%s | sha256sum | base64 | head -c "$USER_PASSWORD_LENGTH" ; echo)
 
-apt-get install apt-utils
-apt-get install software-properties-common
-apt-get install -y htop wget git ntp zsh apt-transport-https ca-certificates curl rsync
+function run_pm_github_script(){
+  bash <(curl -s "$GH_CONTENT/$1/main/$2.sh")
+}
 
-password_length=12
-randompw=$(date +%s | sha256sum | base64 | head -c "$password_length" ; echo)
+function run_setup_script(){
+  run_pm_github_script "docker_server_setup" "$1"
+}
 
-echo ""
-echo "----------"
-echo "Please enter your desired user name"
+function section_split() {
+  printf "\n----------------------------------------\n%s\n\n" "$1"
+}
+
+function section_split_plain() {
+  printf "\n----------------------------------------\n"
+}
+
+section_split "Welcome to 3proxy docker installer!"
+
+section_split_plain
+run_setup_script "remove-file-limits"
+
+section_split_plain "Installing standard debian packages"
+run_setup_script "install_standard_debian_packages"
+
+section_split "Editing sudoers to include Defaults	env_reset,timestamp_timeout=90"
+run_setup_script "increase_sudo_duration"
+
+section_split "Please enter your desired user name"
 read -r DESIRED_USERNAME
+USERS_HOME_FOLDER="/home/$DESIRED_USERNAME"
 
-echo ""
-adduser --disabled-password --gecos "" "$DESIRED_USERNAME"
-sleep 2
-yes "$randompw" | passwd "$DESIRED_USERNAME"
-echo "Added $DESIRED_USERNAME with the randomized password:"
-echo "$randompw"
+section_split "Adding user $DESIRED_USERNAME"
+curl -s "$GH_CONTENT/docker_server_setup/main/add_user.sh" | \
+  bash -s -- "$DESIRED_USERNAME" "$randompw"
 
-adduser "$DESIRED_USERNAME" sudo
+section_split "Setting up $DESIRED_USERNAME to be able to download private github repos"
 
-echo ""
-echo "----------"
-echo "Increasing sudo timeout duration"
-echo "sed -i 's/env_reset/env_reset,timestamp_timeout=90/' /etc/sudoers"
-sed -i 's/env_reset/env_reset,timestamp_timeout=90/' "/etc/sudoers"
+echo "Please enter your github username"
+read -r GITHUB_USERNAME
 
-echo ""
-echo "----------"
-echo "Done, please re-login as $DESIRED_USERNAME using password $randompw now."
+echo "Please enter your github email"
+read -r GITHUB_EMAIL
+
+echo "Please enter your github auth token"
+echo "If you don't have one, can create at https://github.com/settings/tokens being sure to include the right permissions"
+read -r GITHUB_AUTH_TOKEN
+
+curl -s "$GH_CONTENT/docker_server_setup/main/github_setup.sh" | \
+  bash -s "$USERS_HOME_FOLDER" "$GITHUB_USERNAME" "$GITHUB_EMAIL" "$GITHUB_AUTH_TOKEN"
+
+section_split "Setting up docker, docker-compose, and premissions"
+curl -s "$GH_CONTENT/docker_server_setup/main/setup_docker.sh" | \
+  bash -s -- "$DESIRED_USERNAME"
+
+section_split "Done, please re-login as $DESIRED_USERNAME  now using password:"
+echo "$RANDOM_PASSWORD"
