59e85e49ed5f0b746a05fb1a09cf49082e4c2338
Andrew Briscoe <everynothing@gmail.com>
2021-05-20 00:50:49 -0600

    say what apt is doing more

--- install_standard_debian_packages.sh
+++ install_standard_debian_packages.sh
@@ -3,9 +3,20 @@
 export DEBIAN_FRONTEND=noninteractive
 DEBIAN_FRONTEND=noninteractive
 
+function section_split() {
+  printf "\n----------------------------------------\n%s" "$1"
+}
+
+section_split "apt-get update"
 apt-get update
+section_split "apt-get upgrade -y"
 apt-get upgrade -y
 
+section_split "apt-get install -y apt-utils"
 apt-get install -y apt-utils
+
+section_split "apt-get install -y software-properties-common"
 apt-get install -y software-properties-common
+
+section_split "apt-get install -y htop wget git ntp zsh apt-transport-https ca-certificates curl rsync"
 apt-get install -y htop wget git ntp zsh apt-transport-https ca-certificates curl rsync
