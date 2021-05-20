09cae49ff9a3ddba173dc6b80f7bbe9193f67889
Andrew Briscoe <everynothing@gmail.com>
2021-05-20 01:19:47 -0600

    2 newlines

--- add_user.sh
+++ add_user.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-adduser --disabled-password --gecos "" "$DESIRED_USERNAME"
+adduser --disabled-password --gecos "" "$1"
 yes "$2" | passwd "$1"
 
 echo "Added $DESIRED_USERNAME with the randomized password: "
 echo "$2"
 
-adduser "$DESIRED_USERNAME" sudo
+adduser "$1" sudo
